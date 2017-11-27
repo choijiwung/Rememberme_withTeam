bc3c989d2427fdba4486e8d7fae357bf357d2a75
choijiwung <784wldnd@naver.com>
2017-11-27 22:01:21 +0900

    회원가입

--- app/src/main/java/com/rememberme/rememberme/Sign_inActivity.java
+++ app/src/main/java/com/rememberme/rememberme/Sign_inActivity.java
@@ -1,13 +1,13 @@
 package com.rememberme.rememberme;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class Sign_inActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_in);
     }
 }
