f07da40c57dc7e6c666d46fdfc6edd5a113373a4
choijiwung <784wldnd@naver.com>
2017-12-05 10:07:43 +0900

    calendar 추가

---
+++ app/src/main/java/com/rememberme/rememberme/Trip_Date_Activity.java
@@ -0,0 +1,13 @@
+package com.rememberme.rememberme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Trip_Date_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip__date_);
+    }
+}
